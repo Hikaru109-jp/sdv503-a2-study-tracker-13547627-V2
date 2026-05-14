b64883bf17a8c0be72acab603a4b52447faafeb0
岡野陽光 <hikaru@okanoyoukounoMacBook-Air.local>
2026-05-14 21:31:50 +1200

    get input

--- tracker.js
+++ tracker.js
@@ -8,3 +8,9 @@ let topic = "";
 let duration = 0;
 let sessions = [];
 let totalDuration = 0;
+
+async function tracker() {
+    const topic = await ask ("Enter topics: ");
+    const duration = await ask ("Enter times: ")
+    sessions.push({topic, duration});
+}
